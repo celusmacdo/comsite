a1a1d5304db5f848c3d9e6dbfd6c5e2932051794
celusmacdo <1366497414@qq.com>
2018-07-13 18:10:37 +0800

    7.13.1

--- admin/check_adminuser.php
+++ admin/check_adminuser.php
@@ -10,3 +10,13 @@ if(isset($_GET['username'])){
 		echo 0;
 	}
 }
+if(isset($_GET['nickname'])){
+	//查询管理员是否存在
+	$adminuser=db_find('adminuser','nickname="'.$_GET['nickname'].'"');
+	if(empty($adminuser)==false){
+		//如果能找到管理员就echo一个1作为标志返回给ajax代码的区域
+		echo 1;
+	}else{
+		echo 0;
+	}
+}
